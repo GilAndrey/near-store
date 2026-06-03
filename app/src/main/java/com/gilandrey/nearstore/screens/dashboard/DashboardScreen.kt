dfde224485d4cec3dccddfca77072a241a70c64a
Gil Andrey <162456582+GilAndrey@users.noreply.github.com>
2026-06-03 11:12:19 -0300

    bottom menu bar

---
+++ app/src/main/java/com/gilandrey/nearstore/screens/dashboard/DashboardScreen.kt
@@ -0,0 +1,28 @@
+package com.gilandrey.nearstore.screens.dashboard
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.gilandrey.nearstore.R
+
+@Composable
+fun DashboardScreen() {
+    Scaffold(
+        containerColor = colorResource(R.color.black2),
+        bottomBar = { BottomBar() }
+    ) {
+        paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = paddingValues)
+        ) {
+            item { TopBar() }
+        }
+    }
+}
